b71243861f2f5f1ab3c1e8bc1299ed1ae8478c55
Jose Waldo <josferw@gmail.com>
2023-04-20 17:44:12 -0500

    feat: agregue los dos botones para ser mostrados

--- custom_components/src/App.jsx
+++ custom_components/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Button from './components/button/Button'
+import ButtonTwo from './components/buttonborder/ButtonTwo'
 
 function App() {
   return (
-    <>
-      <h1>Hola Mundo</h1>
-    </>
+    <main className='appContainer'>
+      <Button />
+      <ButtonTwo />
+    </main>
   )
 }
 
