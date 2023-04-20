909553636c7dc55a1432482e7ef81908b85818de
Jose Waldo <josferw@gmail.com>
2023-04-20 18:49:53 -0500

    feat: creacion del componente input

--- custom_components/src/App.jsx
+++ custom_components/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import styleApp from './App.module.css';
 import Button from './components/button/Button'
 import ButtonTwo from './components/buttonborder/ButtonTwo'
+import Input from './components/input/Input'
 
 function App() {
   return (
-    <main className='appContainer'>
+    <main className={styleApp.appContainer}>
       <Button />
       <ButtonTwo />
+      <Input className={styleApp.input}/>
     </main>
   )
 }
--- custom_components/src/components/input/Input.jsx
+++ custom_components/src/components/input/Input.jsx
@@ -1,7 +1,11 @@
-function Input() {
+import inputStyle from "./input.module.css";
+
+function Input({className = ""}) {
     return ( 
-        <>
-        </>
+        <div className={`${className} ${inputStyle.inputContainer}`}>
+            <input className={inputStyle.input} type="text" required/>
+            <span className={inputStyle.span}>username</span>
+        </div>
     );
 }
 
